186791e1b7ed3593978d59274843eeab05789613
Jorge Castello <60675751+jorgio-castello@users.noreply.github.com>
2020-09-29 14:18:30 -0700

    Add ref, small changes to styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../assets/Animations.css';
 import { CSSTransition } from 'react-transition-group';
 
 function App() {
+  const welcomeRef = useRef(null);
   const [showWelcomeMessage, setShowWelcomeMessage] = useState(false);
   useEffect(() => {
     setShowWelcomeMessage(true);
   }, []);
   return (
-    <CSSTransition in={showWelcomeMessage} classNames="welcome" timeout={2500}>
-      <div className="flex container px-5 py-64 w-full h-screen max-h-screen justify-center mx-auto items-center text-center">
+    <CSSTransition in={showWelcomeMessage} classNames="welcome" timeout={2500} nodeRef={welcomeRef}>
+      <div ref={welcomeRef} className="flex container px-5 py-64 w-full h-screen max-h-screen justify-center mx-auto items-center text-center">
         <div className="lg:p-20 p-5 rounded shadow mx-auto">
           <h1 className="text-blue-500 font-thin lg:text-4xl text-2xl">Build anything you want.</h1>
-          <p className="font-thin lg:text-base text-md">With Typescript, React, TailwindCSS, and React-Transitions</p>
-          <p className="font-hairline lg:text-2xl text-xl">Template by <a href="https://jorg.io" target="_blank" className="text-teal-400 cursor-pointer hover:text-teal-500">jorgio</a></p>
+          <p className="font-thin lg:text-base text-sm">With Typescript, React, TailwindCSS, and React-Transitions</p>
+          <p className="font-hairline lg:text-2xl text-lg">Template by <a href="https://jorg.io" target="_blank" rel="noopener noreferrer" className="text-teal-400 cursor-pointer hover:text-teal-500">jorgio</a></p>
         </div>
       </div>
     </CSSTransition>
